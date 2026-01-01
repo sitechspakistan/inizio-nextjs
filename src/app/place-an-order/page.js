d0527ed95485aadf99475555c2a06d9ee5de8b8e
webefydesign <webefydesign@gmail.com>
2026-01-02 01:56:53 +0500

    updated

--- src/app/place-an-order/page.js
+++ src/app/place-an-order/page.js
@@ -3,13 +3,13 @@ import OrderForm from "../components/OrderForm"
 
 export default function placeanorder(){
     return(
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
          <SecondHero subheading="Place an Order"
                         headingone={"Place an"}
                         heading="Order"
                         description="Explore our portfolio highlighting exceptional services and innovative solutions, along with the custom promotional products we’ve crafted for clients across industries." />
         
         <OrderForm/>
-        </>
+        </Suspense>
     )
 }
